4dd1a4b54fb3f12ad4aff552e4773a62ebf8401a
annesophie-cls <annesophie.tonneau@protonmail.com>
2026-08-21 12:16:37 +0200

    chore: expose mission related objects

--- altimetry/search/__init__.py
+++ altimetry/search/__init__.py
@@ -3,7 +3,15 @@
 # All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 """Package for the API of the application."""
-from .models import Mission
+from .models import (
+    Mission,
+    MissionProperties,
+    MissionPropertiesLoader,
+    MissionType,
+)
 from .orbit import get_pass_passage_time, get_selected_passes
 
-__all__ = ['get_selected_passes', 'get_pass_passage_time', 'Mission']
+__all__ = [
+    'get_selected_passes', 'get_pass_passage_time', 'Mission', 'MissionType',
+    'MissionProperties', 'MissionPropertiesLoader'
+]
